535078d1b49f0b9f82d5c7597d97f6e0aabd7494
Arturo0214 <osvaldosuarezcruz@gmail.com>
2025-03-05 16:50:03 -0600

    changed serviceRoutes and serviceController.js

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const { getServices, createService, updateService, deleteService } = require("../controllers/serviceController");
-
 const router = express.Router();
+const {
+  getServices,
+  createService,
+  updateService,
+  deleteService,
+  bulkCreateServices
+} = require("../controllers/serviceController");
 
 router.get("/", getServices);
 router.post("/", createService);
 router.put("/:id", updateService);
 router.delete("/:id", deleteService);
+
+router.post("/bulk", bulkCreateServices);
 
 module.exports = router;
