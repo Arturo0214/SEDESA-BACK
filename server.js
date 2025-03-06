cff3bec522a272108c2f18c2dbf4b41519732701
Arturo0214 <osvaldosuarezcruz@gmail.com>
2025-03-05 19:54:32 -0600

    added cors to the server.js

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://duplicidades-sedesa.netlify.app'
+}));
 app.use(express.json());
 
 app.use("/functions", functionRoutes);
