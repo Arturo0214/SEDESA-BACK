a4e2341c199a728e66725d386289571eac8205af
Arturo0214 <osvaldosuarezcruz@gmail.com>
2025-03-06 12:25:41 -0600

    added cors

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors({
   origin: 'https://duplicidades-sedesa.netlify.app'
 }));
+app.use(cors())
 app.use(express.json());
 
 app.use("/functions", functionRoutes);
