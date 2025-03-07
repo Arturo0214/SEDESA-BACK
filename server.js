75693768966eeb1aefdf4cb487398c21d8a3d1b9
Arturo0214 <osvaldosuarezcruz@gmail.com>
2025-03-07 13:15:22 -0600

    added localhost 5173

--- server.js
+++ server.js
@@ -11,9 +11,20 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-app.use(cors({
-  origin: 'https://duplicidades-sedesa.netlify.app'
-}));
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    'https://duplicidades-sedesa.netlify.app',
+    'http://localhost:5173'
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(cors())
 app.use(express.json());
 
