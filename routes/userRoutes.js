51a8058c44645f333a9745147a80004a07db5584
Arturo0214 <osvaldosuarezcruz@gmail.com>
2025-03-23 22:17:49 -0600

    added register url

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { registerUser, loginUser, getMisDatos, getAllUsers, getUserById, requestPasswordReset, resetPassword } = require('../controllers/userController')
 const { protect } = require('../middleware/authMiddleware')
 
-router.post('/', registerUser)
+router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.post('/reset-password', requestPasswordReset);
 router.put("/reset-password/:token", resetPassword);
